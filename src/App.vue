f1555bf73cf25b9c9ddc6796310eb1e9c8ce23f7
Wanderson Silva <wandersonsilva768@gmail.com>
2020-11-08 01:02:24 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>hello world</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+
+    components: {}
+  }
+</script>
+
+<style>
+</style>
