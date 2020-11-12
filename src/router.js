4ad33f3c6e6b70c37089bf968733ff6154b4e4ff
Wanderson Silva <wandersonsilva768@gmail.com>
2020-11-12 02:27:45 -0300

    refactor: remoção de rotas fictícias/inexistentes

--- src/router.js
+++ src/router.js
@@ -1,18 +1,8 @@
 
-const User = {
-  template: '<div>User 1</div>'
-}
-
-const User2 = {
-  template: '<div>User 2</div>'
-}
-
 import LineList from './components/LinesList';
 
 const routes = [
-  { path: '/', component: LineList },
-  { path: '/app', component: User },
-  { path: '/app2', component: User2 }
+  { path: '/', component: LineList }
 ]
 
 export default routes;
