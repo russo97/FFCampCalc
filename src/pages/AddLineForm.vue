5a556655ca20c8cbb92ecf9444e8d72c5610b88a
Wanderson Silva <wandersonsilva768@gmail.com>
2020-11-24 04:32:51 -0300

    feat: adição de fontes e campos de texto do formulário de adição de lines

--- src/pages/AddLineForm.vue
+++ src/pages/AddLineForm.vue
@@ -4,6 +4,12 @@
 
     <div class="form_block">
       <div class="block_item">
+        <input type="text" placeholder="TAG" />
+      </div>
+      <div class="block_item">
+        <input type="text" placeholder="Nome" />
+      </div>
+      <div class="block_item">
         <input type="text" />
       </div>
     </div>
@@ -23,22 +29,42 @@ export default {
     width: 80%;
     height: auto;
     padding: 10px;
-    max-width: 900px;
+    max-width: 400px;
+    border-radius: 4px;
+    background-color: #fff;
+    box-shadow: 0 0 15px 1px rgba(0, 0, 0, 0.4);
 
     .hero {
-      padding: 5px 0;
-      color: $text_color;
+      color: #000;
+      padding: 20px 0;
+      font-size: 14pt;
       text-align: center;
-      letter-spacing: 2px;
+      margin-bottom: 10px;
       text-transform: uppercase;
-      background-color: $base_color;
-      font-family: 'Big Shoulders Stencil Text', cursive;
+      font-family: 'Lato', sans-serif;
     }
 
     .form_block {
       width: 100%;
       height: auto;
-      display: flex;
+      @extend %flex-center;
+      flex-direction: column;
+
+      .block_item {
+        width: 100%;
+        height: auto;
+        padding: 5px;
+        @extend %flex-center;
+
+        input {
+          width: 90%;
+          padding: 15px;
+          font-size: 11pt;
+          color: #2C3E50;
+          border: solid 1px #ccc;
+          font-family: 'Montserrat';
+        }
+      }
     }
   }
 </style>
